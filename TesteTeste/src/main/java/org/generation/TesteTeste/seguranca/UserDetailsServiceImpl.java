32e7b6d40fc068a2c587716dae1e4d878da8c4ca
Esteban <gustavoesteban13@hotmail.com>
2021-03-04 13:26:47 -0300

    ---

--- TesteTeste/src/main/java/org/generation/TesteTeste/seguranca/UserDetailsServiceImpl.java
+++ TesteTeste/src/main/java/org/generation/TesteTeste/seguranca/UserDetailsServiceImpl.java
@@ -1,5 +1,26 @@
 package org.generation.TesteTeste.seguranca;
 
-public class UserDetailsServiceImpl {
+import java.util.Optional;
 
-}
+import org.generation.TesteTeste.model.Usuario;
+import org.generation.TesteTeste.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		Optional<Usuario> user = userRepository.findAllEmail(userName); 
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + "not found."));
+		return user.map(UserDetailsImp::new).get();
+	}
+	
