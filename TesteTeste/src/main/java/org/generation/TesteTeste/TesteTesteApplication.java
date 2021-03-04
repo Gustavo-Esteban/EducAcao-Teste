1cfd570c10a55f636e2b7fff5dfe5a59a74b7972
Esteban <gustavoesteban13@hotmail.com>
2021-03-04 11:02:13 -0300

    teste

---
+++ TesteTeste/src/main/java/org/generation/TesteTeste/TesteTesteApplication.java
@@ -0,0 +1,13 @@
+package org.generation.TesteTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTesteApplication.class, args);
+	}
+
+}
---
+++ TesteTeste/src/test/java/org/generation/TesteTeste/TesteTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.TesteTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
